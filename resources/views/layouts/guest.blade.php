f344b4ed3d29c304653efa24b23a4412df9af125
Javohir070 <qayumovjavohir24@gmail.com>
2024-07-31 15:08:28 +0500

    yuklash kerak

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,6 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="/admin/dist/css/app.css" />
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="login">
         <div class="container sm:px-10">
